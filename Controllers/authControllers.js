7872615d6ae57e6fb560801122e8cf028e737777
why-deepanshux <deepanshusharma12800@gmail.com>
2025-01-27 19:52:34 +0530

    token validity increased

--- Controllers/authControllers.js
+++ Controllers/authControllers.js
@@ -43,7 +43,7 @@ exports.login = async (req, res) => {
     const token = jwt.sign(
       { id: user._id, username: user.username, email: user.email },
       process.env.JWT_SECRET_KEY,
-      { expiresIn: "1h" }
+      { expiresIn: "3h" }
     );
 
     res.json({ message: "Login successful", token });
